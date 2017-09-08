ae4e5ae8f5998442a293ce75333e86b4b18cac51
andrii0574 <0574@ukr.net>
2017-09-08 10:12:23 +0300

    First Project

---
+++ src/JavaTechSkills/Modul1/helloworld.java
@@ -0,0 +1,10 @@
+package JavaTechSkills.Modul1;
+
+/**
+ * Created by Komp on 08.09.2017.
+ */
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("Как я проведу лето 2017");
+    }
+}
